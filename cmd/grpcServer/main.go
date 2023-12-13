57269f81644cf05ca674ec2732a0d347c7feea1b
= <wagnerpalmeira.ti@gmail.com>
2023-12-13 16:43:51 -0300

    Creating server grpc

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/own4rd/go-grpc-example/internal/database"
+	"github.com/own4rd/go-grpc-example/internal/pb"
+	"github.com/own4rd/go-grpc-example/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+	// Read and process own info
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	// OPEN TCP
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
